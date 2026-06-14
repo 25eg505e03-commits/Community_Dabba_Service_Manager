d94d481dd9f58cea9e916bf1bb1a0bf2032f254d
25eg505e03-commits <25eg505e03@anurag.edu.in>
2026-06-14 10:55:55 +0530

    update

--- Dabba-frontend/src/services/api.js
+++ Dabba-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://community-dabba-service-manager.onrender.com"
 
 // Create one reusable Axios connection
 const api = axios.create({
